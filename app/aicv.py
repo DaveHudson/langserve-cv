23573a0eb60e92bf51f9b359d41829733bc1fa6d
Dave Hudson <dave@applification.net>
2024-01-23 09:15:03 +0000

    Remove availability from chain when not required Fixes #1

--- app/aicv.py
+++ app/aicv.py
@@ -5,7 +5,6 @@ from typing import List, Tuple
 from dotenv import load_dotenv
 from langchain.prompts.prompt import PromptTemplate
 from langchain.pydantic_v1 import BaseModel, Field
-from langchain.schema import format_document
 from langchain_community.vectorstores import FAISS, Pinecone
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import ChatPromptTemplate
@@ -54,7 +53,7 @@ CONDENSE_QUESTION_PROMPT = PromptTemplate.from_template(_TEMPLATE)
 
 
 # RAG prompt
-template = """You are an AI designed to emulate the thoughts and views of Dave Hudson. Your responses should be in the first person, as if Dave himself is speaking. Use phrases like "In my view..." or "I believe...".
+TEMPLATE = """You are an AI designed to emulate the thoughts and views of Dave Hudson. Your responses should be in the first person, as if Dave himself is speaking. Use phrases like "In my view..." or "I believe...".
 	Your responses should be based solely on the context provided, which includes Dave's blog posts and his thoughts on various topics.
 	If the question asks about a programming languages that is not in {languages} then your response should always containt the text "I am not famililar with that language".
     If a question is asked that cannot be answered based on the context, respond with "I'm sorry, I don't have any views on that topic yet. Please feel free to email me at dave@applification.net for further discussion."
@@ -66,17 +65,18 @@ template = """You are an AI designed to emulate the thoughts and views of Dave H
 	If the question asks about availability then your response should include {currentAvailability} 
 	answer:
 """
-ANSWER_PROMPT = ChatPromptTemplate.from_template(template)
 
-# DEFAULT_DOCUMENT_PROMPT = PromptTemplate.from_template(template='{page_content}')
-
-
-# def _combine_documents(
-# 	docs, document_prompt=DEFAULT_DOCUMENT_PROMPT, document_separator='\n\n'
-# ):
-# 	"""Combine documents into a single string."""
-# 	doc_strings = [format_document(doc, document_prompt) for doc in docs]
-# 	return document_separator.join(doc_strings)
+TEMPLATE_NO_AVAILABILITY = """You are an AI designed to emulate the thoughts and views of Dave Hudson. Your responses should be in the first person, as if Dave himself is speaking. Use phrases like "In my view..." or "I believe...".
+	Your responses should be based solely on the context provided, which includes Dave's blog posts and his thoughts on various topics.
+	If the question asks about a programming languages that is not in {languages} then your response should always containt the text "I am not famililar with that language".
+    If a question is asked that cannot be answered based on the context, respond with "I'm sorry, I don't have any views on that topic yet. Please feel free to email me at dave@applification.net for further discussion."
+	If a question is asked about a full time job, respond with "I am an I.T contractor operating outside of IR35, full-time employment is not of interest to me at this time."
+	If a question is asked about day rate, respond with "My day rate depends on the specific requirements of the contract."
+	Remember, your goal is to provide a conversational experience that is as close as possible to a real conversation with Dave. Do not invent or assume any views that are not explicitly stated in the context.
+	Context: {context}
+	question: {question}
+	answer:
+"""
 
 
 def _format_chat_history(chat_history: List[Tuple]) -> str:
@@ -100,13 +100,30 @@ _inputs = RunnableMap(
 	| ChatOpenAI(temperature=0)
 	| StrOutputParser(),
 )
-_context = {
-	# 'context': itemgetter('standalone_question') | retriever | _combine_documents,
-	'context': itemgetter('standalone_question') | retriever,
-	'currentAvailability': itemgetter('standalone_question') | availabilityRetriever,
-	'languages': itemgetter('standalone_question') | languagesRetriever,
-	'question': lambda x: x['standalone_question'],
-}
+
+
+def get_context_dict(standalone_question):
+	if 'availability' in standalone_question:
+		return {
+			'context': itemgetter('standalone_question') | retriever,
+			'currentAvailability': itemgetter('standalone_question')
+			| availabilityRetriever,
+			'languages': itemgetter('standalone_question') | languagesRetriever,
+			'question': lambda x: x['standalone_question'],
+		}
+	else:
+		return {
+			'context': itemgetter('standalone_question') | retriever,
+			'languages': itemgetter('standalone_question') | languagesRetriever,
+			'question': lambda x: x['standalone_question'],
+		}
+
+
+def get_template_dict(standalone_question):
+	if 'availability' in standalone_question:
+		return ChatPromptTemplate.from_template(TEMPLATE)
+	else:
+		return ChatPromptTemplate.from_template(TEMPLATE_NO_AVAILABILITY)
 
 
 # LCEL Chain
@@ -121,6 +138,10 @@ class ChatHistory(BaseModel):
 
 
 conversational_qa_chain = (
-	_inputs | _context | ANSWER_PROMPT | ChatOpenAI() | StrOutputParser()
+	_inputs
+	| get_context_dict({'question': lambda x: x['standalone_question']})
+	| get_template_dict({'question': lambda x: x['standalone_question']})
+	| ChatOpenAI()
+	| StrOutputParser()
 )
 chain = conversational_qa_chain.with_types(input_type=ChatHistory)
